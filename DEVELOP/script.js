9635cd38d82032e44254626e404bf9a76db4472e
Justin Garcia <justingarcia1901@gmail.com>
2024-03-26 19:50:42 -0400

    time updates and conversion based on 24 hour clock

--- DEVELOP/script.js
+++ DEVELOP/script.js
@@ -27,20 +27,26 @@ $(function () {
       localStorage.setItem(timeBlock.id, textarea.value);
     });
   });
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // Update time block colors
+  function updateTimeBlockColors() {
+    const currentHour = dayjs().hour();
+
+    document.querySelectorAll('.time-block').forEach((block) => {
+      let blockHour = parseInt(block.id.split('-')[1], 10);
+
+      let blockHour24 = blockHour < 9 ? blockHour + 12 : blockHour;
+
+      block.classList.remove('past', 'present', 'future');
+
+      // Add updated time class
+      if (blockHour24 < currentHour) {
+        block.classList.add('past');
+      } else if (blockHour24 === currentHour) {
+        block.classList.add('present');
+      } else {
+        block.classList.add('future');
+      }
+    });
+  }
 });
